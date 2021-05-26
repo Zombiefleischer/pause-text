b4408a3657dd7fdde8b68a38235ac69979ce5940
Freeze020 <bdevries2011@gmail.com>
2021-05-26 09:33:33 +0200

    separated out the functions to different files for more clarity

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Simple module to change the image of the pause icon shown when the game is paused, to make your UI just that little more unique.
+
+Change the image using a picker and enjoy.
---
+++ scripts/settings.js
@@ -0,0 +1,22 @@
+import IconPicker from "./IconPicker.js";
+
+export const registerSettings = function () {
+    game.settings.register("pause-icon", "path", {
+        name: game.i18n.format("PAUSEICON.path_name"),
+        hint: game.i18n.format("PAUSEICON.path_hint"),
+        scope: "world",
+        config: true,
+        default: "[data] icons/svg/clockwork.svg",
+        type: IconPicker.Image,
+        onChange: () => window.location.reload()
+    });
+    game.settings.register("pause-icon", "opacity", {
+        name: game.i18n.format("PAUSEICON.opacity_name"),
+        hint: game.i18n.format("PAUSEICON.opacity_hint"),
+        scope: "world",
+        config: true,
+        default: 50,
+        type: Number,
+        onChange: () => window.location.reload()
+    });
+}
