5c3c514c51758ab5f14ec10f49a5e788561137a9
Freeze020 <bdevries2011@gmail.com>
2021-08-18 14:20:53 +0200

    Now allows for None as icon option.

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,12 @@ Hooks.on("renderPause", function () {
     const path = game.settings.get("pause-icon", "path");
     const opacity = parseInt(game.settings.get("pause-icon", "opacity")) / 100;
     const speed = game.settings.get("pause-icon", "speed") + "s linear 0s infinite normal none running rotation";
-    $("#pause.paused img").attr("src", path);
-    $("#pause.paused img").css({"opacity": opacity, "-webkit-animation": speed});
+    if(path === "None") {
+        $("#pause.paused img").hide();
+    }
+    else {
+        $("#pause.paused img").attr("src", path);
+        $("#pause.paused img").css({"opacity": opacity, "-webkit-animation": speed});
+    }
     $("#pause.paused h3").text(game.settings.get("pause-icon", "text"));
 });
