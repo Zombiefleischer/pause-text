c3d90c42ad88f2d64081d1cac53e3043f742c616
Jesus Nava <jesusnavadidp@gmail.com>
2023-05-28 16:39:23 +0200

    added support for picture alignment added support for text font family reordered properties to reflect form reordered fields in form to group

--- scripts/script.js
+++ scripts/script.js
@@ -4,21 +4,25 @@ Hooks.on('setup', () => {
 });
 Hooks.on("renderPause", function () {
   if ($("#pause").attr("class") !== "paused") return;
-  const path = game.settings.get("pause-text", "allSettings").path;
-  const opacity = game.settings.get("pause-text", "allSettings").opacity / 100;
-  let speed = game.settings.get("pause-text", "allSettings").speed + "s";
   const message = game.settings.get('pause-text', 'allSettings').text;
   const lines = message.split(/\n/);
   const text = lines[Math.floor(Math.random() * lines.length)];
-  console.log('pause-text | ' + text);
-  const dimensionX = game.settings.get("pause-text", "allSettings").dimensionX;
-  const dimensionY = game.settings.get("pause-text", "allSettings").dimensionY;
-  const top = `${-16 - (dimensionY - 128) / 2}px`;
-  const left = `calc(50% - ${dimensionX / 2}px)`;
+  const fontFamily= game.settings.get("pause-text", "allSettings").fontFamily;
   const textColor = game.settings.get("pause-text", "allSettings").textColor;
   const shadow = game.settings.get("pause-text", "allSettings").shadow;
   const fontSize = game.settings.get("pause-text", "allSettings").fontSize;
   const size = `${(text.length * fontSize * 90 / 12) + 70}px 100px`;
+
+  const path = game.settings.get("pause-text", "allSettings").path;
+  const opacity = game.settings.get("pause-text", "allSettings").opacity / 100;
+  let speed = game.settings.get("pause-text", "allSettings").speed + "s";
+  const dimensionX = game.settings.get("pause-text", "allSettings").dimensionX;
+  const dimensionY = game.settings.get("pause-text", "allSettings").dimensionY;
+  const iconSpacingY = game.settings.get("pause-text", "allSettings").iconSpacingY;
+  const top = `${-16 - (dimensionY - 128) / 2 + iconSpacingY}px`;
+  const left = `calc(50% - ${dimensionX / 2}px)`;
+
+
   if (path === "None" || dimensionX === 0 || dimensionY === 0) {
     $("#pause.paused img").hide();
   }
@@ -32,14 +36,15 @@ Hooks.on("renderPause", function () {
       $("#pause.paused img").css({ "top": top, "left": left, "width": dimensionX, "height": dimensionY, "opacity": opacity, "-webkit-animation": speed });
     }
   }
+
   if (isNewerVersion(game.version, "10")) {
     $("#pause.paused figcaption").text(text);
     if (text.length !== 0 && shadow) {
       $("#pause.paused").css({ "background-size": size });
-      $("#pause.paused figcaption").css({ "color": textColor, "font-size": `${fontSize}em` });
+      $("#pause.paused figcaption").css({ "color": textColor, "font-size": `${fontSize}em`, "font-family": `${fontFamily}` });
     }
     else if (text.length !== 0 && !shadow) {
-      $("#pause.paused figcaption").css({ "color": textColor, "font-size": `${fontSize}em` });
+      $("#pause.paused figcaption").css({ "color": textColor, "font-size": `${fontSize}em`, "font-family": `${fontFamily}` });
       $("#pause.paused figcaption").css({ "color": textColor });
       $("#pause.paused").css("background", "none");
     }
@@ -51,10 +56,10 @@ Hooks.on("renderPause", function () {
     $("#pause.paused h3").text(text);
     if (text.length !== 0 && shadow) {
       $("#pause.paused").css({ "background-size": size });
-      $("#pause.paused h3").css({ "color": textColor, "font-size": `${fontSize}em` });
+      $("#pause.paused h3").css({ "color": textColor, "font-size": `${fontSize}em`, "font-family": `${fontFamily}` });
     }
     else if (text.length !== 0 && !shadow) {
-      $("#pause.paused h3").css({ "color": textColor, "font-size": `${fontSize}em` });
+      $("#pause.paused h3").css({ "color": textColor, "font-size": `${fontSize}em`, "font-family": `${fontFamily}` });
       $("#pause.paused h3").css({ "color": textColor });
       $("#pause.paused").css("background", "none");
     }
