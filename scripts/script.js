4c1ff2a4d60ba8f0ce0343c98769e2454f885cd2
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2024-01-23 00:31:25 +0100

    working sync
    Signed-off-by: Zombiefleischer <zombiefleischer@zombiefleischer.de>

--- scripts/script.js
+++ scripts/script.js
@@ -1,81 +1,153 @@
-import {registerSettings} from "./settings.js"
-Hooks.on('setup', () => {
-    registerSettings();
-});
-Hooks.on("renderPause", function () {
-  if ($("#pause").attr("class") !== "paused") return;
-  const message = game.settings.get('pause-text', 'allSettings').text;
-  const lines = message.split(/\n/);
-  const text = lines[Math.floor(Math.random() * lines.length)];
-  const fontFamily= game.settings.get("pause-text", "allSettings").fontFamily;
-  const textColor = game.settings.get("pause-text", "allSettings").textColor;
-  const shadow = game.settings.get("pause-text", "allSettings").shadow;
-  const fontSize = game.settings.get("pause-text", "allSettings").fontSize;
-  const size = `${(text.length * fontSize * 90 / 12) + 70}px 100px`;
+import { registerSettings } from "./settings.js";
+let socket;
 
-  const path = game.settings.get("pause-text", "allSettings").path;
-  const opacity = game.settings.get("pause-text", "allSettings").opacity / 100;
-  let speed = game.settings.get("pause-text", "allSettings").speed + "s";
-  const reverse = game.settings.get("pause-text", "allSettings").reverse;
-  const dimensionX = game.settings.get("pause-text", "allSettings").dimensionX;
-  const dimensionY = game.settings.get("pause-text", "allSettings").dimensionY;
-  const iconSpacingY = game.settings.get("pause-text", "allSettings").iconSpacingY;
+Hooks.on("setup", () => {
+  registerSettings();
+});
+
+Hooks.once("socketlib.ready", () => {
+  socket = socketlib.registerModule("pause-text");
+  socket.register("selectAndBroadcastPauseText", selectAndBroadcastPauseText);
+  socket.register("displayPauseText", displayPauseText);
+});
+
+Hooks.on("renderPause", async function () {
+  if ($("#pause").attr("class") !== "paused") return;
+
+  // Get all settings
+  const settings = game.settings.get("pause-text", "allSettings");
+
+  // Get image settings
+  const path = settings.path;
+  const opacity = settings.opacity / 100;
+  let speed = settings.speed + "s";
+  const reverse = settings.reverse;
+  const dimensionX = settings.dimensionX;
+  const dimensionY = settings.dimensionY;
+  const iconSpacingY = settings.iconSpacingY;
   const top = `${-16 - (dimensionY - 128) / 2 + iconSpacingY}px`;
   const left = `calc(50% - ${dimensionX / 2}px)`;
 
+  // Get random text
+  const message = settings.allText;
+  const lines = message.split(/\n/);
+  settings.selectedText = lines[Math.floor(Math.random() * lines.length)];
 
+  // Get sync settings and open socket
+  if ($("#pause").attr("class") === "paused" && settings.sync) {
+    await socket.executeAsGM("selectAndBroadcastPauseText", settings);
+  } else {
+    displayPauseText(settings);
+  }
+
+  // Change the displayed image
   if (path === "None" || dimensionX === 0 || dimensionY === 0) {
     $("#pause.paused img").hide();
-  }
-  else {
+  } else {
     $("#pause.paused img").attr("src", path);
     if (isNewerVersion(game.version, "10")) {
       if (reverse) {
-        $("#pause.paused img").css({ "top": top, "left": left, "width": dimensionX, "height": dimensionY, "opacity": opacity, "--fa-animation-duration": speed, "--fa-animation-direction": "reverse" });
+        $("#pause.paused img").css({
+          top: top,
+          left: left,
+          width: dimensionX,
+          height: dimensionY,
+          opacity: opacity,
+          "--fa-animation-duration": speed,
+          "--fa-animation-direction": "reverse",
+        });
       } else {
-        $("#pause.paused img").css({ "top": top, "left": left, "width": dimensionX, "height": dimensionY, "opacity": opacity, "--fa-animation-duration": speed });
+        $("#pause.paused img").css({
+          top: top,
+          left: left,
+          width: dimensionX,
+          height: dimensionY,
+          opacity: opacity,
+          "--fa-animation-duration": speed,
+        });
       }
-    }
-    else {
+    } else {
       if (reverse) {
         speed += " linear 0s infinite reverse none running rotation";
-        $("#pause.paused img").css({ "top": top, "left": left, "width": dimensionX, "height": dimensionY, "opacity": opacity, "-webkit-animation": speed });
-      }
-      else {
+        $("#pause.paused img").css({
+          top: top,
+          left: left,
+          width: dimensionX,
+          height: dimensionY,
+          opacity: opacity,
+          "-webkit-animation": speed,
+        });
+      } else {
         speed += " linear 0s infinite normal none running rotation";
-        $("#pause.paused img").css({ "top": top, "left": left, "width": dimensionX, "height": dimensionY, "opacity": opacity, "-webkit-animation": speed });
+        $("#pause.paused img").css({
+          top: top,
+          left: left,
+          width: dimensionX,
+          height: dimensionY,
+          opacity: opacity,
+          "-webkit-animation": speed,
+        });
       }
     }
   }
+});
 
+async function selectAndBroadcastPauseText(settings) {
+  if (game.user.isGM) {
+    settings.randomText = settings.selectedText;
+    await socket.executeForEveryone("displayPauseText", settings);
+  }
+}
+
+function displayPauseText(settings) {
+  // Get text settings
+  const text = settings.selectedText;
+  const fontFamily = settings.fontFamily;
+  const textColor = settings.textColor;
+  const shadow = settings.shadow;
+  const fontSize = settings.fontSize;
+  const size = `${(text.length * fontSize * 90) / 12 + 70}px 100px`;
+
+  // Change pause text
   if (isNewerVersion(game.version, "10")) {
     $("#pause.paused figcaption").text(text);
     if (text.length !== 0 && shadow) {
       $("#pause.paused").css({ "background-size": size });
-      $("#pause.paused figcaption").css({ "color": textColor, "font-size": `${fontSize}em`, "font-family": `${fontFamily}` });
-    }
-    else if (text.length !== 0 && !shadow) {
-      $("#pause.paused figcaption").css({ "color": textColor, "font-size": `${fontSize}em`, "font-family": `${fontFamily}` });
-      $("#pause.paused figcaption").css({ "color": textColor });
+      $("#pause.paused figcaption").css({
+        color: textColor,
+        "font-size": `${fontSize}em`,
+        "font-family": `${fontFamily}`,
+      });
+    } else if (text.length !== 0 && !shadow) {
+      $("#pause.paused figcaption").css({
+        color: textColor,
+        "font-size": `${fontSize}em`,
+        "font-family": `${fontFamily}`,
+      });
+      $("#pause.paused figcaption").css({ color: textColor });
+      $("#pause.paused").css("background", "none");
+    } else {
       $("#pause.paused").css("background", "none");
     }
-    else {
-      $("#pause.paused").css("background", "none");
-    }
-  }
-  else {
+  } else {
     $("#pause.paused h3").text(text);
     if (text.length !== 0 && shadow) {
       $("#pause.paused").css({ "background-size": size });
-      $("#pause.paused h3").css({ "color": textColor, "font-size": `${fontSize}em`, "font-family": `${fontFamily}` });
-    }
-    else if (text.length !== 0 && !shadow) {
-      $("#pause.paused h3").css({ "color": textColor, "font-size": `${fontSize}em`, "font-family": `${fontFamily}` });
-      $("#pause.paused h3").css({ "color": textColor });
+      $("#pause.paused h3").css({
+        color: textColor,
+        "font-size": `${fontSize}em`,
+        "font-family": `${fontFamily}`,
+      });
+    } else if (text.length !== 0 && !shadow) {
+      $("#pause.paused h3").css({
+        color: textColor,
+        "font-size": `${fontSize}em`,
+        "font-family": `${fontFamily}`,
+      });
+      $("#pause.paused h3").css({ color: textColor });
       $("#pause.paused").css("background", "none");
-    }
-    else {
+    } else {
       $("#pause.paused").css("background", "none");
     }
   }
-});
+}
--- scripts/settings.js
+++ scripts/settings.js
@@ -1,101 +1,104 @@
 import { messages } from "./messages.js";
 class PauseIconSubmenu extends FormApplication {
-    constructor() {
-        super({});
+  constructor() {
+    super({});
+  }
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["form"],
+      popOut: true,
+      width: "550",
+      height: "auto",
+      template: `/modules/pause-text/templates/settings-submenu.html`,
+      id: "pause-text-settings-submenu",
+      title: game.i18n.format("PAUSEICON.settingsFormHeader"),
+      resizable: false,
+    });
+  }
+  activateListeners(html) {
+    super.activateListeners(html);
+    const saveButton = $(".pt-settings-save", html);
+    saveButton[0].addEventListener("click", async function () {
+      await game.settings.set("pause-text", "allSettings", {
+        path: $(".pt-path", html).val(),
+        opacity: Number($(".pt-opacity", html).val()),
+        dimensionX: Number($(".pt-dimensionX", html).val()),
+        dimensionY: Number($(".pt-dimensionY", html).val()),
+        iconSpacingY: Number($(".pt-iconSpacingY", html).val()),
+        allText: $(".pt-text", html).val(),
+        sync: $(".pt-sync").prop("checked"),
+        fontFamily: $(".pt-fontFamily", html).val(),
+        textColor: $(".pt-text-color", html).val(),
+        shadow: $(".pt-shadow").prop("checked"),
+        fontSize: $(".pt-font-size").val(),
+        speed: $(".pt-speed").val(),
+        reverse: $(".pt-reverse").prop("checked"),
+      });
+      window.location.reload();
+    });
+    const picker = $(".pt-picker-button", html);
+    picker[0].addEventListener("click", async function () {
+      new FilePicker({
+        type: "image",
+        callback: async function (imagePath) {
+          $(".pt-path").val(imagePath);
+        },
+      }).render(true);
+    });
+  }
+  getData() {
+    let source = game.settings.get("pause-text", "allSettings");
+    if (foundry.utils.isObjectEmpty(source)) {
+      source = {
+        path: "icons/svg/clockwork.svg",
+        opacity: 50,
+        dimensionX: 128,
+        dimensionY: 128,
+        iconSpacingY: 0,
+        allText: messages,
+        sync: true,
+        fontFamily: "",
+        textColor: "#EEEEEE",
+        shadow: true,
+        fontSize: 2,
+        speed: "5",
+        reverse: false,
+      };
     }
-    static get defaultOptions() {
-        return mergeObject(super.defaultOptions, {
-            classes: ['form'],
-            popOut: true,
-            width: "550",
-            height: "auto",
-            template: `/modules/pause-text/templates/settings-submenu.html`,
-            id: 'pause-text-settings-submenu',
-            title: game.i18n.format("PAUSEICON.settingsFormHeader"),
-            resizable: false,
-            
-        });
-    }
-    activateListeners(html) {
-        super.activateListeners(html);
-        const saveButton = $(".pt-settings-save", html);
-        saveButton[0].addEventListener("click", async function(){
-            await game.settings.set("pause-text", "allSettings", {
-                path: $(".pt-path", html).val(),
-                opacity: Number($(".pt-opacity", html).val()),
-                dimensionX: Number($(".pt-dimensionX", html).val()),
-                dimensionY: Number($(".pt-dimensionY", html).val()),
-                iconSpacingY: Number($(".pt-iconSpacingY", html).val()),
-                text: $(".pt-text", html).val(),
-                fontFamily:$(".pt-fontFamily", html).val(),
-                textColor: $(".pt-text-color", html).val(),
-                shadow: $(".pt-shadow").prop("checked"),
-                fontSize: $(".pt-font-size").val(),
-                speed: $(".pt-speed").val(),
-                reverse: $(".pt-reverse").prop("checked")
-            });
-            window.location.reload();
-        });
-        const picker = $(".pt-picker-button", html);
-        picker[0].addEventListener("click", async function(){
-            new FilePicker({
-                type: "image",
-                callback: async function (imagePath) {
-                  $(".pt-path").val(imagePath);
-                }}).render(true);
-        })
-    }
-    getData() {
-        let source = game.settings.get("pause-text", "allSettings");
-        if (foundry.utils.isObjectEmpty(source)) {
-            source = {
-                path: "icons/svg/clockwork.svg",
-                opacity: 50,
-                dimensionX: 128,
-                dimensionY: 128,
-                iconSpacingY:0,
-                text: messages,
-                fontFamily:"",
-                textColor: "#EEEEEE",
-                shadow: true,
-                fontSize: 2,
-                speed: "5",
-                reverse: false
-            };
-        }
-        return source;
-    }
-    // _updateObject(event, formData) {
-    //     console.log(`Alternative Pause Icon | Saving compendia sources:`);
-    //     console.log(formData);
-    //     //return game.settings.set("pause-text", "allSettings", formData);
-    // }
+    return source;
+  }
+  // _updateObject(event, formData) {
+  //     console.log(`Alternative Pause Icon | Saving compendia sources:`);
+  //     console.log(formData);
+  //     //return game.settings.set("pause-text", "allSettings", formData);
+  // }
 }
 export const registerSettings = function () {
-    game.settings.register("pause-text", "allSettings", {
-        scope: 'world',
-        config: false,
-        type: Object,
-        default: {
-            path: "icons/svg/clockwork.svg",
-            opacity: 50,
-            dimensionX: 128,
-            dimensionY: 128,
-            iconSpacingY:0,
-            text: messages,
-            fontFamily:"",
-            textColor: "#EEEEEE",
-            shadow: true,
-            fontSize: 2,
-            speed: "5",
-            reverse: false
-        },
-    });
-    game.settings.registerMenu("pause-text", "allSettings", {
-        name: game.i18n.format("PAUSEICON.settings"),
-        label: game.i18n.format("PAUSEICON.settingsButton"),
-        icon: 'fas fa-atlas',
-        type: PauseIconSubmenu,
-        restricted: true
-    })
+  game.settings.register("pause-text", "allSettings", {
+    scope: "world",
+    config: false,
+    type: Object,
+    default: {
+      path: "icons/svg/clockwork.svg",
+      opacity: 50,
+      dimensionX: 128,
+      dimensionY: 128,
+      iconSpacingY: 0,
+      allText: messages,
+      sync: true,
+      fontFamily: "",
+      textColor: "#EEEEEE",
+      shadow: true,
+      fontSize: 2,
+      speed: "5",
+      reverse: false,
+    },
+  });
+  game.settings.registerMenu("pause-text", "allSettings", {
+    name: game.i18n.format("PAUSEICON.settings"),
+    label: game.i18n.format("PAUSEICON.settingsButton"),
+    icon: "fas fa-atlas",
+    type: PauseIconSubmenu,
+    restricted: true,
+  });
 };
