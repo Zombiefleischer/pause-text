6644b27b7ceacc99ada26450b1be9d11e9f98475
Zombiefleischer <44850408+Zombiefleischer@users.noreply.github.com>
2025-06-25 01:59:44 +0200

    feat!: Upgrade to Foundry V13, also almost complete rewrite
    Signed-off-by: Zombiefleischer <44850408+Zombiefleischer@users.noreply.github.com>

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+import {registerSettings} from "./settings.js";
+import { setPauseUI } from "./pause-text.js";
+
+export const MODULE_ID = "pause-text";
+
+Hooks.on("init", async () => {
+  registerSettings();
+});
+
+
+
+Hooks.on("renderGamePause", function (_,html,options) {
+  if (options.cssClass === "paused") {
+    setTimeout(() => {
+      setPauseUI(html);
+    });
+  }
+});
+
+// Sadly doesn't set the image on the first load
+// Hooks.on("pauseGame", (paused, html) => {
+//   if (paused) {
+//     setTimeout(() => {
+//       setPauseUI(html);
+//     }, 1);
+//   }
+// })
